e95f3126c9789550d8bcc6a3f03c6088c45eb65b
Zarfleck <152239451+Zarfleck@users.noreply.github.com>
2025-09-05 16:15:16 -0700

    Add files via upload

---
+++ lang/messages/en/user.js
@@ -0,0 +1,13 @@
+export const userMessages = {
+    // Page title
+    gameTitle: "Game",
+    
+    // Input section
+    inputTitle: "Enter a number between 3 and 7",
+    inputPlaceholder: "Enter here",
+    enterButton: "Enter",
+    
+    // Game messages
+    loseMessage: "You cant remember anything, You Lose!",
+    winMessage: "Exelent Memory, You Win!"
+};
